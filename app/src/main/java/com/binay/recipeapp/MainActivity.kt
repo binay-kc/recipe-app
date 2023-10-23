89310a30409efcc3da2598737be22ca995b58fa1
BInay KC <kc.binay6174@gmail.com>
2023-10-22 21:23:10 -0400

    added navigation and fragments #4

--- app/src/main/java/com/binay/recipeapp/MainActivity.kt
+++ app/src/main/java/com/binay/recipeapp/MainActivity.kt
@@ -1,11 +1,72 @@
 package com.binay.recipeapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.PorterDuff
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import com.binay.recipeapp.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    val imageList = intArrayOf(R.drawable.nav_home, R.drawable.nav_fav, R.drawable.nav_more)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.viewPager.isUserInputEnabled = false
+
+        val fragments: MutableList<Fragment> = ArrayList()
+        fragments.add(HomeFragment())
+        fragments.add(FavoriteFragment())
+        fragments.add(MoreFragment())
+
+        //code to change selected tab color
+        binding.tabLayout.addOnTabSelectedListener(object : OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                tab.icon!!.setColorFilter(ContextCompat.getColor(this@MainActivity, R.color.primary_color), PorterDuff.Mode.SRC_IN)
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab) {
+                tab.icon!!.setColorFilter(ContextCompat.getColor(this@MainActivity, R.color.default_tab_color), PorterDuff.Mode.SRC_IN)
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab) {}
+        })
+
+        val pagerAdapter = MyPagerAdapter(this, fragments)
+        binding.viewPager.adapter = pagerAdapter
+
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+            // Set tab text or leave it empty if you want to display only icons
+            tab.text = ""
+            tab.icon = ContextCompat.getDrawable(this, imageList[position])
+        }.attach()
+    }
+
+    class MyPagerAdapter(fragmentActivity: FragmentActivity?, fragments: List<Fragment>) :
+        FragmentStateAdapter(fragmentActivity!!) {
+        private val fragments: List<Fragment>
+
+        init {
+            this.fragments = fragments
+        }
+
+        override fun createFragment(position: Int): Fragment {
+            return fragments[position]
+        }
+
+        override fun getItemCount(): Int {
+            return fragments.size
+        }
     }
 }
